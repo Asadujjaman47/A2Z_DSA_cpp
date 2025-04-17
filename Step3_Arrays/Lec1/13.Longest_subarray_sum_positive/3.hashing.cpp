2bd2775bcc644b2502beedfffa6579368ad73d44
Asad <asad1@itconquest.com>
2025-04-17 14:22:39 +0600

    Step3_Lec1_II

---
+++ Step3_Arrays/Lec1/13.Longest_subarray_sum_positive/3.hashing.cpp
@@ -0,0 +1,54 @@
+// Longest Subarray with given Sum K(Positives)
+// Better Approach(Using Hashing):
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int> &a, long long k)
+{
+    int n = a.size(); // size of the array.
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // calculate the prefix sum tull index i;
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k;
+        long long rem = sum - k;
+
+        // calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end())
+        {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        // Finally, update the map checking the conditinos:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> a = {2, 3, 5, 1, 9};
+    long long k = 10;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+
+// Time Complexity: O(N) or O(N*logN)
+// Space Complexity: O(N)
